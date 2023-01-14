af317d9e9bfae06f2c09940bebe231974081090f
Marat <maratussr@gmail.com>
2023-01-15 00:44:38 +0400

    first step

---
+++ src/test/java/com/marat/test/AvitoTest.java
@@ -0,0 +1,25 @@
+package com.marat.test;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.sleep;
+
+public class AvitoTest {
+
+    @BeforeAll
+    public static void beforeAll() {
+        Configuration.browserSize = "1366x768";
+    }
+
+    @Test
+    public void testForGit() {
+        Selenide.open("https://www.avito.ru/");
+        sleep(4000);
+        $(".top-rubricator-rubricatorButton-SoKyQ").click();
+        sleep(2000);
+    }
+}
